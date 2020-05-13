740c458e1df3bd2bb3292a3aec144b063bf4508c
programador <contacto@gmail.com>
2020-05-13 18:30:06 -0400

    Agregar accion nuevo alumno

--- Practicaweb_Alumno/Practicaweb_Alumno/Controllers/AlumnoController.cs
+++ Practicaweb_Alumno/Practicaweb_Alumno/Controllers/AlumnoController.cs
@@ -3,13 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Practicaweb_Alumno.Models;
 
 namespace Practicaweb_Alumno.Controllers
 {
     public class AlumnoController : Controller
     {
         // GET: Alumno
-        public ActionResult Index()
+        public ActionResult Inicio()
+        {
+            using (BD_AlumnoEntities db = new BD_AlumnoEntities())
+            {
+                //List<Alumno> lista = db.Alumno.Where(a=>a.Edad>18).ToList();
+                return View(db.Alumno.ToList());
+            }               
+        }
+
+        public ActionResult Agregar()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Agregar(Alumno a)
         {
             return View();
         }
